066bf310d72a8a27e92708e63a8b83d717d2b64f
eric <eric@429a0ea4-229a-47fc-af7a-729dd6d1e645>
2010-11-24 06:42:35 +0000

    more stuff that wasnt added before
    git-svn-id: svn://svn.direnetworks.com/wh/site/trunk@28 429a0ea4-229a-47fc-af7a-729dd6d1e645

---
+++ js/geocode.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+      myAddress = $("div#map_canvas").text();
+   geocodeInitialize();
+   codeAddress(myAddress);
+});
+
+var geocoder;
+var map;
+function geocodeInitialize() {
+   geocoder = new google.maps.Geocoder();
+   var latlng = new google.maps.LatLng(-34.397, 150.644);
+   var myOptions = {
+      zoom: 13,
+      center: latlng,
+      mapTypeId: google.maps.MapTypeId.HYBRID,
+   }
+   map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+}
+
+function codeAddress(address) {
+   //var address = document.getElementById("eafp").value;
+   if (geocoder) {
+      geocoder.geocode( { 'address': address}, function(results, status) {
+	    if (status == google.maps.GeocoderStatus.OK) {
+		  map.setCenter(results[0].geometry.location);
+		  var marker = new google.maps.Marker({
+			map: map, 
+			position: results[0].geometry.location
+		  });
+	    } else {
+		  codeAddress("Crabby's Boat House, 200 Main St # 101A, Huntington Beach, CA 92648-8102");
+	    }
+      });
+   }
+}
