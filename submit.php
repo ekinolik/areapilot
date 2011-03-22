34d4624a8e19cae147cc37189f85a17f33fe37f4
Eric Kinolik <eric@direnetworks.com>
2011-03-21 23:27:52 -0700

    BUG[8]: changed the page title to "Post an Event"

--- submit.php
+++ submit.php
@@ -2,7 +2,7 @@
 
 if ( ! defined('config')) require('config.php');
 
-$TITLE .= ' - Submit';
+$TITLE .= ' - Post an Event';
 
 $CODE = $CURRENT_DIR.'code/submit.php';
 $OUTPUT = $CURRENT_DIR.'html_output/submit.php';
