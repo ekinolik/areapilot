ea9ae573258d2e3a606c94d6b22912e65c6f96d0
Eric Kinolik <eric@direnetworks.com>
2011-03-23 20:48:13 -0700

    BUG[26]: category on submit page is vague
    Category now defaults to "Select a Category" which isn't a valid category.
    Categories are now in the format of "Category > Sub-Category"

--- html_output/submit.php
+++ html_output/submit.php
@@ -2,15 +2,15 @@
 
 if ( ! defined('HTMLCLASS')) require(LIB_DIR.'HTML.php');
 
-$cat_opts = '';
+$cat_opts = '<option value="">Select a Category</option>'."\n";
 for ($i = 0, $iz = count($category->category); $i < $iz; ++$i) {
    $cat = &$category->category[$i];
    $cat_opts .= '<optgroup label="'.$cat['parent']['title'].'">'."\n";
-   $cat_opts .= '  <option value="'.$cat['parent']['id'].'">All</option>'."\n";
+   $cat_opts .= '  <option value="'.$cat['parent']['id'].'">'.$cat['parent']['title'].' > All</option>'."\n";
 
    for ($j = 0, $jz = count($cat['children']); $j < $jz; ++$j) {
       $child = &$cat['children'][$j];
-      $cat_opts .= '  <option value="'.$child['id'].'">'.$child['title'].'</option>'."\n";
+      $cat_opts .= '  <option value="'.$child['id'].'">'.$cat['parent']['title'].' &gt; '.$child['title'].'</option>'."\n";
    }
 
    $cat_opts .= '</optgroup>'."\n";
