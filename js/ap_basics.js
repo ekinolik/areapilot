7404a067a52c6198b406c482dde27a26f35bd495
Eric Kinolik <eric@direnetworks.com>
2011-03-30 16:17:21 -0700

    BUG[60]: attend event gives js error
    Should have just been json.error

--- js/ap_basics.js
+++ js/ap_basics.js
@@ -143,11 +143,11 @@ function vote(id, a, t) {
 }
 
 function readRating(json) {
-   if (json.error.error.length > 0) {
+   if (json.error.length > 0) {
       if (json.error.errno == 4) {
 	 blockThis("body",$("#modal-login"),nothing(),false,true);
       } else {
-	 alert(json.error.error);
+	 alert(json.error);
       }
       return false;
    }
