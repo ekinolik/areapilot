49d51038d906e1259e747c887ed93041fd8a2818
Eric Kinolik <eric@direnetworks.com>
2011-04-01 11:58:49 -0700

    BUG[XX]: facebook admins were incorrect

--- lib/HTML.php
+++ lib/HTML.php
@@ -1029,6 +1029,8 @@ class HTML {
 	 $html .= $s.'<meta property="og:site_name" content="'.$fbm['site_name'].'" />'."\n";
       if (isset($fbm['admins']))
 	 $html .= $s.'<meta property="fb:admins" content="'.$fbm['admins'].'" />'."\n";
+      if (isset($fbm['app_id']))
+	 $html .= $s.'<meta property="fb:app_id" content="'.$fbm['app_id'].'" />'."\n";
 
       return $html;
    }
