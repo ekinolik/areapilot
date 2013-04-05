450e5439306c49f66c4804455bcd6385495ab93b
Eric Kinolik <eric@direnetworks.com>
2013-04-04 22:08:36 -0700

    Added a config settings template file.  Removed .htaccess file in place of a .htaccess template file

---
+++ config_settings_template.php
@@ -0,0 +1,32 @@
+<?php
+
+define('CONFIG_SETTINGS', 1);
+
+define('PRODUCTION', TRUE);
+
+define('PROTOCOL', 'http://');
+define('SPROTOCOL', 'https://');
+define('DOMAIN', '');
+define('IMG_DOMAIN', '');
+define('COOKIE_DOMAIN', '');
+define('ROOT_DIR', '');
+define('IMG_DIR', '');
+
+define('MAIL_RELAY', '');
+define('MAIL_HOSTNAME', '');
+define('CONTACT_EMAIL', '');
+define('CONTACT_NAME', '');
+
+define('FACEBOOK_ADMINS', '');
+
+define('SESS_RAND', '');
+
+define('DB_HOST', '');
+define('DB_NAME', '');
+define('DB_USER', '');
+define('DB_PASS', '');
+define('DB_TYPE', 'pgsql');
+
+define('ERROR_DEBUG', 0);
+
+?>
